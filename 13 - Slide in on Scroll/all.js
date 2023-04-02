aa1fa830c979b8f9bf194a0784d4a4672f25181d
a733181 <a733181@gmail.com>
2023-04-02 14:26:21 +0800

    修改 13

--- 13 - Slide in on Scroll/all.js
+++ 13 - Slide in on Scroll/all.js
@@ -1,16 +1,18 @@
 "use strict";
 function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
+    let timeout;
+    return function (...args) {
+        const context = this;
+        const later = function () {
             timeout = null;
             if (!immediate)
                 func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
+        let callNow = immediate && !timeout;
+        if (typeof timeout === 'number') {
+            clearTimeout(timeout);
+        }
+        timeout = window.setTimeout(later, wait);
         if (callNow)
             func.apply(context, args);
     };
@@ -18,11 +20,9 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 function checkSlide() {
     sliderImages.forEach((sliderImage) => {
-        // half way through the image
-        const slideInAt = window.scrollY + window.innerHeight - sliderImage.height / 2;
-        // bottom of the image
+        const y = window.scrollY + window.innerHeight - sliderImage.height / 2;
         const imageBottom = sliderImage.offsetTop + sliderImage.height;
-        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isHalfShown = y > sliderImage.offsetTop;
         const isNotScrolledPast = window.scrollY < imageBottom;
         if (isHalfShown && isNotScrolledPast) {
             sliderImage.classList.add('active');
