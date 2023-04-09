5bcf952b6af91a06354b016c6cdb72cffb4e49b4
a733181 <a733181@gmail.com>
2023-04-09 14:56:03 +0800

    新增 20

---
+++ 20 - Speech Detection/index.ts
@@ -0,0 +1,41 @@
+// 使用類型斷言告訴 TypeScript SpeechRecognition 的類型
+const SpeechRecognition =
+  (window as any).SpeechRecognition ||
+  (window as any).webkitSpeechRecognition ||
+  null;
+
+if (SpeechRecognition) {
+  // 在支持的情況下，定義 SpeechRecognition 變數
+  const recognition = new SpeechRecognition();
+  // 下一步操作 SpeechRecognition 對象
+} else {
+  // 在不支持的情況下，給出錯誤處理或提示
+  console.error('SpeechRecognition is not supported in this browser.');
+}
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'zh-TW';
+
+let p = document.createElement('p');
+const words = document.querySelector<HTMLDivElement>('.words')!;
+words.appendChild(p);
+
+recognition.addEventListener('result', (event) => {
+  const transcript = Array.from(event.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join('');
+
+  const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '');
+  p.textContent = poopScript;
+
+  if (event.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
